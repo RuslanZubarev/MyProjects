fa0922cebdebb4c39060c4951e07b8a43dc9b380
Руслан Зубарев <ruslanzubarev01@gmail.com>
2021-09-27 17:23:23 +0000

    Update Printer.java

--- 3.3/src/Printer.java
+++ 3.3/src/Printer.java
@@ -1,6 +1,6 @@
 public class Printer {
     private static String queue;
-    private int amountPage = 0;
+    private int amountPage = 1;
     private int pendingPagesCount;
     private int totalAmountPage;
     private int a;
@@ -10,22 +10,14 @@ public class Printer {
     }
 
     public void append(String name, String text) {
-        append(name, text, 0);
+        append(name, text, 1);
     }
 
     public void append(String name, String text, int amountPage) {
-        totalAmountPage = totalAmountPage + amountPage;
         pendingPagesCount = pendingPagesCount + amountPage;
-        queue = "";
         queue = queue + "\n" + "Имя: " + name +
-                       "\n" + "Текст: " + text +
-                       "\n" + "Количество страниц: " + amountPage;
-        for (a = 0; amountPage >= 0; amountPage = --amountPage) {
-            System.out.println("Количество страниц в очереди: " + amountPage);
-
-        }
-        System.out.println("Очередь очищена");
-
+                "\n" + "Текст: " + text +
+                "\n" + "Количество страниц: " + amountPage;
     }
 
     public void clear() {
@@ -34,13 +26,13 @@ public class Printer {
     }
 
     public void print() {
+        totalAmountPage = totalAmountPage + pendingPagesCount;
         if (queue.isEmpty()) {
             System.out.println("Очередь очищена");
         } else {
             System.out.println(queue);
             System.out.println("Общее количество распечатанных страниц: " + getTotalAmountPage());
             System.out.println("Общее количество страниц: " + getPendingPagesCount());
-
         }
     }
 
