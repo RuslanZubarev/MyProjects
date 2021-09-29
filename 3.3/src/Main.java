8706d763439b6f5b68a4bae232c330cde400c4a3
Руслан Зубарев <ruslanzubarev01@gmail.com>
2021-09-29 15:10:25 +0000

    Update Main.java

--- 3.3/src/Main.java
+++ 3.3/src/Main.java
@@ -4,11 +4,14 @@ public class Main {
         printer.append("Руководство пользователя", "Бери и пользуйся.", 3);
         printer.print();
         System.out.println();
+        printer.clear();
         printer.append("Java для чайников", "учим Java", 4);
         printer.print();
         System.out.println();
+        printer.clear();
         printer.append("Курсовая работа", "текст Курсовой работы", 5);
         printer.print();
         System.out.println();
+        printer.clear();
     }
 }
