c89ed5bc36cea8af09b9e1b91655b91f28ca81f7
ruslan <ruslanzubarev01@gmail.com>
2021-10-09 21:59:19 +0400

    Homework 4.4 / made some fixes

--- 04_Encapsulation/04_Copy/src/CargoInfo.java
+++ 04_Encapsulation/04_Copy/src/CargoInfo.java
@@ -6,6 +6,15 @@ public class CargoInfo {
     private final String regNumber;
     private final boolean isFragile;
 
+    public CargoInfo(){
+        dimensions = 10;
+        weight = 15;
+        deliveryAdress = "N/A";
+        canOverturn = true;
+        regNumber = "AAA001";
+        isFragile = false;
+    }
+
     public CargoInfo(int dimensions, int weight, String deliveryAdress, boolean canOverturn,
                      String regNumber, boolean isFragile) {
         this.dimensions = dimensions;
@@ -20,12 +29,24 @@ public class CargoInfo {
         return dimensions;
     }
 
+    public CargoInfo setDimensions(int dimensions) {
+        return  new CargoInfo(dimensions, weight, deliveryAdress, canOverturn, regNumber, isFragile);
+    }
+
     public int getWeight() {
         return weight;
     }
 
+    public CargoInfo setWeight(int weight) {
+        return  new CargoInfo(dimensions, weight, deliveryAdress, canOverturn, regNumber, isFragile);
+    }
+
     public String getDeliveryAdress() {
         return deliveryAdress;
+    }
+
+    public CargoInfo setDeliveryAdress(String deliveryAdress) {
+        return  new CargoInfo(dimensions, weight, deliveryAdress, canOverturn, regNumber, isFragile);
     }
 
     public boolean isCanOverturn() {
