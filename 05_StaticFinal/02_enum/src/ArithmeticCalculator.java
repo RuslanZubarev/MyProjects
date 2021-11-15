84c4a1ea0168e91822f5495a2e48ad75d8867170
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-11-15 19:04:44 +0400

    made an enum, ArithmeticCalculator class

--- 05_StaticFinal/02_enum/src/ArithmeticCalculator.java
+++ 05_StaticFinal/02_enum/src/ArithmeticCalculator.java
@@ -1,9 +1,10 @@
 public class ArithmeticCalculator {
-
     public final Operation type;
 
-    public ArithmeticCalculator(int a, int b, Operation type) {
-        this.type = type;
+    int a = 5;
+    int b = 4;
+
+    public ArithmeticCalculator(int a, int b,) {
         this.a = a;
         this.b = b;
     }
