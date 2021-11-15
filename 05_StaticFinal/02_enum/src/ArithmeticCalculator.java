fbdc04dd32565030b47482e0a5430ed5533980ba
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-11-15 19:48:51 +0400

    modified ArifthmeticCalculator class

--- 05_StaticFinal/02_enum/src/ArithmeticCalculator.java
+++ 05_StaticFinal/02_enum/src/ArithmeticCalculator.java
@@ -1,16 +1,26 @@
 public class ArithmeticCalculator {
-    public final Operation type;
-
     int a = 5;
     int b = 4;
 
-    public ArithmeticCalculator(int a, int b,) {
+
+    public final Operation type;
+
+
+    public ArithmeticCalculator(int a, int b, Operation type) {
         this.a = a;
         this.b = b;
+        this.type = type;
     }
 
 
+    public ArithmeticCalculator(Operation type) {
+        this.type = type;
+    }
+
     public Operation getType() {
         return type;
+    }
+
+    public void calculate() {
     }
 }
