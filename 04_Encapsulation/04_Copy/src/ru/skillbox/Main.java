198f77e0da20c47c965d38df416d0cda2e6df118
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-11-15 13:01:57 +0400

    add some info in Main

--- 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ 04_Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -6,11 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Dimensions dimensions = new Dimensions(1, 1, 1);
         Dimensions dimensions2 = dimensions.setWidth(2).setHeight(2).setLenght(2);
-        System.out.println(dimensions2);
+        System.out.println("До изменения: \n" + dimensions);
+        System.out.println();
+        System.out.println("После изменения: \n" + dimensions2);
         System.out.println();
         Cargo cargo = new Cargo(dimensions2, 10, "Address", true, "AA001", false);
         Cargo cargo1 = cargo.setWeight(30).setDeliveryAddress("Another address");
-        System.out.println(cargo1);
+        System.out.println("Информация о грузе №1: \n" + cargo);
+        System.out.println();
+        System.out.println("Информация о грузе №2: \n" + cargo1);
 
     }
 }
