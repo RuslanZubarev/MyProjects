59476abf2dc2640afb46d9425dd767d3da4e4520
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-11-21 16:06:23 +0400

    made int field int count instead of Integer count class/ made the code from addCount method simpler

--- 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -2,7 +2,7 @@ public class Container {
     private int count;
 
     public void addCount(int value) {
-        count = count + value;
+        count += value;
     }
 
     public int getCount() {
