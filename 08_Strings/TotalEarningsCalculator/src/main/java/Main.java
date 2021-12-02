5a9093deb79c2dc225969cbb5f217d87a0c81c49
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-02 23:42:00 +0400

    first commit

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,17 @@
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
-
+        String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+        String digits = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isDigit(i)) {
+                digits += i;
+            }
+        }
+    }
 }
+
+
+//TODO: напишите ваш код, результат вывести в консоль
+
