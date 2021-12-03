c4da892163be72faf223761aa11020c5d77acb49
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-03 15:39:30 +0400

    wrote the code

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,14 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String digits = "";
-        for (int i = 0; i < text.length(); i++) {
-            if (Character.isDigit(i)) {
-                digits += i;
+
+        System.out.println(getIncome(text));
+    }
+
+    public static String getIncome(String string) {
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char str = string.charAt(i);
+            if (Character.isDigit(str)) {
+                result.append(str);
+            } else if (!result.toString().equals("")) {
+                count += Integer.parseInt(result.toString());
+                result = new StringBuilder();
+            }
+            if (count < 0) {
+                return "";
             }
         }
+        return Integer.toString(count);
     }
 }
 
