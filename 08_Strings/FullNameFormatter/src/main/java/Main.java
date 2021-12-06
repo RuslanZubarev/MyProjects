0230d3dd7aeadd6e8dd67c351279ba0ebda9ee6d
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-06 16:21:13 +0400

    initial

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Введите ФИО");
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("0")) {
-                break;
-
-           //     String fs = String.format("Фамилия: %s%nИмя: %s%nОтчество: %s");
+        String input = scanner.nextLine();
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char str = input.charAt(i);
+            if (Character.isWhitespace(str)) {
+                count++;
             }
-            //TODO:напишите ваш код тут, результат вывести в консоль.
-            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
         }
+        //TODO:напишите ваш код тут, результат вывести в консоль.
+        //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+//        }
     }
 
+
 }
+//            System.out.printf("Фамилия: %s%n" +
+//                    "Имя: %s%n" +
+//                    "Отчество: %s", input);
+
+//    public static String getName(String name) {
+//        return "";
+//    }
