e4652276e4c1c8ead20e8fd3c075f758b713e4ea
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-06 18:24:43 +0400

    added some code

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -6,24 +6,30 @@ public class Main {
         System.out.println("Введите ФИО");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        input.trim();
         int count = 0;
         for (int i = 0; i < input.length(); i++) {
             char str = input.charAt(i);
             if (Character.isWhitespace(str)) {
                 count++;
             }
+
+//            int start = input.indexOf(' ', 1);
+//            int end = input.indexOf(' ', 5);
+//            String encoding = input.substring(start, end);
+//            System.out.println(encoding);
         }
         //TODO:напишите ваш код тут, результат вывести в консоль.
         //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
 //        }
+//        String firstName = "";
+//        String lastName = "";
+//        String thirdName = "";
+//        String tamplate = "Фамилия: %s\n" +
+//                "Имя: %s\n " +
+//                "Отчество: %s";
+//        System.out.printf(tamplate, firstName, lastName, thirdName);
     }
-
-
 }
-//            System.out.printf("Фамилия: %s%n" +
-//                    "Имя: %s%n" +
-//                    "Отчество: %s", input);
 
-//    public static String getName(String name) {
-//        return "";
-//    }
+
