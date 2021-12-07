b06f945ca6f58c0f26cbaf29456294e9d3756c78
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-07 15:46:08 +0400

    work done

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -3,33 +3,45 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Введите ФИО");
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        input.trim();
-        int count = 0;
-        for (int i = 0; i < input.length(); i++) {
-            char str = input.charAt(i);
-            if (Character.isWhitespace(str)) {
-                count++;
+        while (true) {
+            String input = scanner.nextLine().trim();
+            int start = input.indexOf(' ');
+            int end = input.lastIndexOf(' ');
+            if (input.equals("0")) {
+                break;
+            }
+            int spaceCount = 0;
+            int i = 0;
+            for (; i < input.length(); i++) {
+                char str = input.charAt(i);
+
+                if (Character.isWhitespace(str) || (!Character.UnicodeBlock.of(str).equals(Character.UnicodeBlock.CYRILLIC)) && input.charAt(i) != '-') {
+                    spaceCount++;
+                }
             }
 
-//            int start = input.indexOf(' ', 1);
-//            int end = input.indexOf(' ', 5);
-//            String encoding = input.substring(start, end);
-//            System.out.println(encoding);
+            if ((spaceCount != 2) || (input.isBlank() || Character.isDigit(i))) {
+                System.out.println("Введенная строка не является ФИО");
+            } else {
+                String lastName = input.substring(0, start).trim();
+
+                String firstName = input.substring(lastName.length() + 1, end).trim();
+
+                String patronymic = input.substring(start + firstName.length() + 1).trim();
+
+                String templay = "Фамилия: %s%nИмя: %s%nОтчество: %s%n";
+                System.out.printf(templay, lastName, firstName, patronymic);
+
+            }
+
         }
-        //TODO:напишите ваш код тут, результат вывести в консоль.
-        //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-//        }
-//        String firstName = "";
-//        String lastName = "";
-//        String thirdName = "";
-//        String tamplate = "Фамилия: %s\n" +
-//                "Имя: %s\n " +
-//                "Отчество: %s";
-//        System.out.printf(tamplate, firstName, lastName, thirdName);
     }
 }
+
+
+//TODO:напишите ваш код тут, результат вывести в консоль.
+//При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
 
 
