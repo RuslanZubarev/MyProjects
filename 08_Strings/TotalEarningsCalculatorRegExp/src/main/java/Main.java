a4b7e0e43db167dd8b62f8babce291c15dd61493
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-17 02:01:16 +0400

    initial

--- 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(calculateSalarySum(name));
 
-  public static void main(String[] args) {
+    }
 
-  }
-
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
-  }
+    public static int calculateSalarySum(String text) {
+        String regex = "[0-9]*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        int sum = 0;
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            int result = Integer.parseInt(text.substring(start, end));
+            sum += result;
+        }
+        return sum;
+    }
 
 }
