e6565cfa2581900ab33680cdf525ba7edc4b17be
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-17 22:48:29 +0400

    initial

--- 08_Strings/SplitText/src/main/java/Main.java
+++ 08_Strings/SplitText/src/main/java/Main.java
@@ -1,12 +1,16 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        String name1 = "";
+        System.out.println(splitTextIntoWords(name1));
+    }
 
-  }
-
-  public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
-  }
-
+    public static String splitTextIntoWords(String text) {
+        String regex = "[^a-zA-Z'’]+"; // создаем шаблон.
+        return text.replaceAll(regex, System.lineSeparator()).trim(); //применяем шаблон в первом параметре и вместо указанных в нем значений(. , ; : ' ' '\n') используем перенос строки, а так же убираем пробелы перед и после слова.
+    }
 }
