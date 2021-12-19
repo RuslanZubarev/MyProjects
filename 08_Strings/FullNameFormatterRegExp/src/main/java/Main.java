34f6f1be2785ad94fcc87c9691fa367bb69be5de
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-19 17:05:14 +0400

    initial

--- 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
+++ 08_Strings/FullNameFormatterRegExp/src/main/java/Main.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            String regex = "^([А-Я]{1}[а-яё]+(-[А-Я]{1}[а-яё]+)?)\\s([А-Я]{1}[а-яё]+)\\s([А-Я]{1}[а-яё]+)$";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                System.out.println("Фамилия: " + matcher.group(1));
+                System.out.println("Имя: " + matcher.group(3));
+                System.out.println("Отчество: " + matcher.group(4));
+            } else {
+                System.out.println("Введенная строка не является ФИО");
+                break;
+            }
+
+            //TODO:напишите ваш код тут, результат вывести в консоль.
+            //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+        }
     }
-  }
 
 }
