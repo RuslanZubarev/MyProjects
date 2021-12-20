c12613b17916fe43257a5855bd46cad2ff1136ee
Ruslan Zubarev <ruslanzubarev01@gmail.com>
2021-12-20 17:56:43 +0400

    first commit

--- 08_Strings/PhoneCleaner/src/main/java/Main.java
+++ 08_Strings/PhoneCleaner/src/main/java/Main.java
@@ -1,17 +1,34 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(formatPhoneNumber(input));
+//    while (true) {
+//      String input = scanner.nextLine();
+//      String regex = "[^0-9]";
+//      if (input.equals("0")) {
+//        break;
+//      }
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+//    }
     }
-  }
+
+    public static String formatPhoneNumber(String input) {
+        String regex = "[^0-9]"; //создаем регулярку, которая будет отсеивать все НЕ цифры
+        String secondRegex = "[0-9]{10,11}"; // регулярка проверяет количество цифр, оно должно быть 10 или 11
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        Pattern secondPattern = Pattern.compile(secondRegex);
+        Matcher secondMatcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            System.out.println("Неверный формат номера");
+        }
+        return input.replaceAll(regex, ""); // удаляем НЕ символы найденые регуляркой
+    }
 
 }
